9ec6bbedfb60ffcc43dbd0b7051d3e397b8c7b5d
Fabmoi <poulainfabian@gmail.com>
2021-09-03 17:32:48 +0200

    up

--- clic.js
+++ clic.js
@@ -10,7 +10,6 @@ var counterClick = 0;
 
 //////////////////////////////////////// AUGMENTER LES CLICS ////////////////////////////////////////
 $(document).ready(function(){
-
     document.getElementById("click-here").addEventListener("click", function() {
         counterClick++;
         document.getElementById("click-counter").innerHTML = counterClick;
@@ -18,54 +17,34 @@ $(document).ready(function(){
 });
 //////////////////////////////////////// RESET LES CLICS ////////////////////////////////////////
 $(document).ready(function(){
-
     document.getElementById("reset-button").addEventListener("click", function() {
         counterClick = 0;
         document.getElementById("click-counter").innerHTML = counterClick;
     });
 });
-//////////////////////////////////////// DÉPENSER LES CLICS ////////////////////////////////////////
+//////////////////////////////////////// ACHAT PREMIER BONUS ////////////////////////////////////////
 $(document).ready(function(){
     var autoClick = document.getElementById('click-here');
-
-    document.querySelector(".first-stuff-10").addEventListener("click", function() {
-        counterClick = counterClick -10;
-        document.getElementById("click-counter").innerHTML = counterClick;
-        setInterval(function(){
-            autoClick.click();
-        },200);
-    });
+        document.getElementById("first-bonus").addEventListener("click", function() {
+            counterClick = counterClick -20;
+            document.getElementById("click-counter").innerHTML = counterClick;
+            setInterval(function(){
+                autoClick.click();
+            },750);
+        });
 });
 //////////////////////////////////////// BOUTON BONUS DÉSACTIVÉ ET ACTIVÉ ////////////////////////////////////////
-
 function updateNumberClick(number){
-    var button = document.getElementById("btn");
-    document.getElementById("click-counter").innerHTML = number;
-    if (number >=0 && number <9) {
-        button.disabled = true;
-    }
-    else {
-        button.disabled = false;
-    }
+    var button = document.getElementById("first-bonus");
+        document.getElementById("click-counter").innerHTML = number;
+        if (number <19) {
+            button.disabled = true;
+        }
+        else {
+            button.disabled = false;
+        }
 }
 ////////////////////////////////////////  ////////////////////////////////////////
-
-
-
-/* 
-function updateNumberClick (autoNumberOne){
-    var autoClick = document.getElementById("click-here");
-    document.getElementById("click-counter").innerHTML = autoNumberOne;
-    setInterval(function(){
-        autoClick.click();
-    },200);
-}  */
-
-
-/* const button = document.getElementById("btn");
-    if (counterClick < 10) {
-        button.disabled = true;
-    }    */
 
 
 
